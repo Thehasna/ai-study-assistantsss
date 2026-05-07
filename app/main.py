bc65fa154111fed067469a7bc8185fc751fbb39d
Thehasna <thehasnajayakody@gmail.com>
2026-05-08 00:48:59 +0200

    Added main program file

---
+++ app/main.py
@@ -0,0 +1,11 @@
+print("AI Study Assistant Started")
+
+while True:
+
+    user_input = input("Ask something: ")
+
+    if user_input.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    print("You said:", user_input)
